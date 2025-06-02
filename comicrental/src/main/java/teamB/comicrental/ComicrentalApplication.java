43949731d709331e216d2d74010c8c061b19890a
“takeda.nao <example@ex.com>
2025-06-02 16:16:36 +0900

    comic

---
+++ comicrental/src/main/java/teamB/comicrental/ComicrentalApplication.java
@@ -0,0 +1,13 @@
+package teamB.comicrental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComicrentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComicrentalApplication.class, args);
+	}
+
+}
---
+++ comicrental/src/test/java/teamB/comicrental/ComicrentalApplicationTests.java
@@ -0,0 +1,13 @@
+package teamB.comicrental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComicrentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
