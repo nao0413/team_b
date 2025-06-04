31ffee94dc13772f90d9030da41e4825c2cd8530
tokunaga.miyu <example@ex.com>
2025-06-04 16:37:03 +0900

    サブスクリプションのコード

--- comicrental/src/main/java/teamB/comicrental/subscription/repository/SubscMapper.java
+++ comicrental/src/main/java/teamB/comicrental/subscription/repository/SubscMapper.java
@@ -20,21 +20,21 @@ public interface SubscMapper {
     @Select("select customer_id,customer_name,is_subscribed from customer where customer_id=#{customerId} limit 1")
     public Optional<SubscModel> findById(@Param("customerId") int customerId);
 
-    @Update("update customer set
-            is_subscribed=#{is_subscribed},
-            credit_number=#{credit_number}
-            credit_name=#{credit_name}
-            credit_date=#{credit_date}
-            security_code=#{security_code}
-            subsciption_start_date=#{subscription_start_date} 
-            WHERE customer_id=#{customer_id}")
-            public void updateSubscriptionInfo(SubscModel subscModel
-                @Param("customer_id")int customer_id,
-                @Param("is_subscribed")boolean is_subscribed,
-                @Param("credit_number")String credit_number,
-                @Param("credit_name")String credit_name,
-                @Param("credit_code")String credit_code,
-                @Param("security_code")String security_code,
-                @Param("subsciption_start_date")OffsetDateTime subscription_start_date;
-                );
+    // @Update("update customer set
+    //         is_subscribed=#{is_subscribed},
+    //         credit_number=#{credit_number}
+    //         credit_name=#{credit_name}
+    //         credit_date=#{credit_date}
+    //         security_code=#{security_code}
+    //         subsciption_start_date=#{subscription_start_date} 
+    //         WHERE customer_id=#{customer_id}")
+    //         public void updateSubscriptionInfo(SubscModel subscModel
+    //             @Param("customer_id")int customer_id,
+    //             @Param("is_subscribed")boolean is_subscribed,
+    //             @Param("credit_number")String credit_number,
+    //             @Param("credit_name")String credit_name,
+    //             @Param("credit_code")String credit_code,
+    //             @Param("security_code")String security_code,
+    //             @Param("subsciption_start_date")OffsetDateTime subscription_start_date;
+    //             );
 }
