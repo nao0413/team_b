ddddfdebd8cbf7e61f2b191a2e09dcba349aa6b8
tokunaga.miyu <example@ex.com>
2025-06-04 16:36:28 +0900

    ファイルの整理とサブスクリプションのコードの一部

---
+++ comicrental/src/main/java/teamB/comicrental/subscription/model/SubscPageModel.java
@@ -0,0 +1,49 @@
+package teamB.comicrental.subscription.model;
+
+import java.util.List;
+
+import teamB.comicrental.subscription.repository.SubscModel;
+
+/**
+ * SubscriptionPageModel.java
+ */
+
+public class SubscPageModel {
+    public String title;
+    public List<SubscModel> list;
+    private Integer id;
+    private String name;
+    private boolean subscribed;
+    
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public List<SubscModel> getList() {
+        return list;
+    }
+    public void setList(List<SubscModel> list) {
+        this.list = list;
+    }
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public boolean isSubscribed() {
+        return subscribed;
+    }
+    public void setSubscribed(boolean subscribed) {
+        this.subscribed = subscribed;
+    }
+    
+}
