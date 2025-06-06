7d54d55f4529e4e049fe20a00ac44b1e2ff6248f
tokunaga.miyu <example@ex.com>
2025-06-06 16:42:13 +0900

    漫画一覧ページのテンプレート・コードの作成

---
+++ comicrental/src/main/java/teamB/comicrental/comictable/model/ComicPageModel.java
@@ -0,0 +1,14 @@
+package teamB.comicrental.comictable.model;
+
+import java.util.List;
+import teamB.comicrental.comictable.repository.ComicModel;
+
+
+/**
+ * SubscriptionPageModel.java
+ */
+
+public class ComicPageModel {
+    public String title;
+    public List<ComicModel> list;
+}
