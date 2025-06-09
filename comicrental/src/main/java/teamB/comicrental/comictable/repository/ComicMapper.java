6066b2a6131d91e346e898c61639268d77ad66dc
tokunaga.miyu <example@ex.com>
2025-06-09 17:32:47 +0900

    今日の作業分

--- comicrental/src/main/java/teamB/comicrental/comictable/repository/ComicMapper.java
+++ comicrental/src/main/java/teamB/comicrental/comictable/repository/ComicMapper.java
@@ -21,4 +21,6 @@ public interface ComicMapper {
     //上記のに合わせて、rentalテーブルも合体させて情報を取得するSQL
     @Select("select c.comic_id,c.title,c.author,c.explanatory,c.category_id,ca.category_name,c.comic_image,CASE WHEN r.rental_status='renting' THEN TRUE ELSE FALSE END AS is_rented from comic c LEFT JOIN category ca ON c.category_id=ca.category_id LEFT JOIN rental r ON c.comic_id=r.comic_id AND r.customer_id=#{customer_id} AND r.rental_status='renting' ORDER BY c.comic_id")
     public List<ComicModel> findAllComicsWithCategoryAndRentalStatus(@Param("customer_id")int customerId);
+
+    // @Select("select c.comic_id,c.title,c.author,c.explanatory,c.category_id,ca.category_name,")
 }
