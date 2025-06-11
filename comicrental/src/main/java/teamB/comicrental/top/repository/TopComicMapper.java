5264bb8e989b2563fa0d9119bc94ea9376b08e54
“takeda.nao <example@ex.com>
2025-06-11 13:42:46 +0900

    あ

--- comicrental/src/main/java/teamB/comicrental/top/repository/TopComicMapper.java
+++ comicrental/src/main/java/teamB/comicrental/top/repository/TopComicMapper.java
@@ -2,7 +2,6 @@ package teamB.comicrental.top.repository;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
-
 import teamB.comicrental.top.model.Comic;
 
 import java.util.List;
@@ -10,19 +9,47 @@ import java.util.List;
 @Mapper
 public interface TopComicMapper {
 
-    @Select("SELECT title, author, comic_image AS comicImage, rentaltimes, genre, arrival_date AS arrivalDate FROM comic ORDER BY rentaltimes DESC LIMIT 5")
+    @Select("""
+        SELECT title, author, comic_image AS comicImage, rentaltimes,
+               category AS genre, arrival_date AS arrivalDate
+        FROM comic
+        ORDER BY rentaltimes DESC
+        LIMIT 5
+    """)
     List<Comic> findTopComics();
 
-    @Select("SELECT title, author, comic_image AS comicImage, rentaltimes, genre, arrival_date AS arrivalDate FROM comic ORDER BY arrival_date DESC LIMIT 5")
+    @Select("""
+        SELECT title, author, comic_image AS comicImage, rentaltimes,
+               category AS genre, arrival_date AS arrivalDate
+        FROM comic
+        ORDER BY arrival_date DESC
+        LIMIT 5
+    """)
     List<Comic> findRecentComics();
 
-    @Select("SELECT title, author, comic_image AS comicImage, rentaltimes, genre, arrival_date AS arrivalDate FROM comic WHERE genre = #{genre} ORDER BY rentaltimes DESC")
+    @Select("""
+        SELECT title, author, comic_image AS comicImage, rentaltimes,
+               category AS genre, arrival_date AS arrivalDate
+        FROM comic
+        WHERE category = #{genre}
+        ORDER BY rentaltimes DESC
+    """)
     List<Comic> findComicsByGenre(String genre);
 
-    @Select("SELECT title, author, comic_image AS comicImage, rentaltimes, genre, arrival_date AS arrivalDate FROM comic WHERE title = #{title} LIMIT 1")
+    @Select("""
+        SELECT title, author, comic_image AS comicImage, rentaltimes,
+               category AS genre, arrival_date AS arrivalDate
+        FROM comic
+        WHERE title = #{title}
+        LIMIT 1
+    """)
     Comic findComicByTitle(String title);
 
-    @Select("SELECT title, author, comic_image AS comicImage, rentaltimes, genre, arrival_date AS arrivalDate FROM comic WHERE title ILIKE #{title}")
-List<Comic> findComicsByTitleLike(String title);
-
-} 
+    @Select("""
+        SELECT title, author, comic_image AS comicImage, rentaltimes,
+               category AS genre, arrival_date AS arrivalDate
+        FROM comic
+        WHERE title ILIKE #{title}
+    """)
+    List<Comic> findComicsByTitleLike(String title);
+}
