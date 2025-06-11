68bf44eae0c2483b728bae6bd382b9dbdcc373b2
“takeda.nao <example@ex.com>
2025-06-11 17:23:53 +0900

    あ

--- comicrental/src/main/java/teamB/comicrental/top/model/Comic.java
+++ comicrental/src/main/java/teamB/comicrental/top/model/Comic.java
@@ -1,20 +1,71 @@
 package teamB.comicrental.top.model;
 
+import java.time.LocalDate;
+
 public class Comic {
+    private int comic_id;              // 一意のID（必要なら）
     private String title;
     private String author;
     private String comicImage;
     private int rentaltimes;
+    private String genre;             // category_id AS genre に対応
+    private LocalDate arrivalDate;    // arrival_date AS arrivalDate に対応
 
-    public String getTitle() { return title; }
-    public void setTitle(String title) { this.title = title; }
+    // --- Getter & Setter ---
 
-    public String getAuthor() { return author; }
-    public void setAuthor(String author) { this.author = author; }
+    public int getComic_id() {
+        return comic_id;
+    }
 
-    public String getComicImage() { return comicImage; }
-    public void setComicImage(String comicImage) { this.comicImage = comicImage; }
+    public void setComic_id(int comic_id) {
+        this.comic_id = comic_id;
+    }
 
-    public int getRentaltimes() { return rentaltimes; }
-    public void setRentaltimes(int rentaltimes) { this.rentaltimes = rentaltimes; }
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getComicImage() {
+        return comicImage;
+    }
+
+    public void setComicImage(String comicImage) {
+        this.comicImage = comicImage;
+    }
+
+    public int getRentaltimes() {
+        return rentaltimes;
+    }
+
+    public void setRentaltimes(int rentaltimes) {
+        this.rentaltimes = rentaltimes;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public LocalDate getArrivalDate() {
+        return arrivalDate;
+    }
+
+    public void setArrivalDate(LocalDate arrivalDate) {
+        this.arrivalDate = arrivalDate;
+    }
 }
