5ce26eec90f820c043ebbf1c19176859354a5d05
“takeda.nao <example@ex.com>
2025-06-12 13:38:38 +0900

    a

--- comicrental/src/main/java/teamB/comicrental/home/controller/HomeController.java
+++ comicrental/src/main/java/teamB/comicrental/home/controller/HomeController.java
@@ -4,8 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import teamB.comicrental.top.repository.TopComicMapper;
+
 import teamB.comicrental.top.model.Comic;
+import teamB.comicrental.top.repository.TopComicMapper;
 
 import java.util.List;
 
@@ -13,12 +14,14 @@ import java.util.List;
 public class HomeController {
 
     @Autowired
-    private TopComicMapper comicMapper;
+private TopComicMapper comicMapper;  // 変数名を統一
 
-    @GetMapping("/home")
+@GetMapping("/home")
 public String showHomePage(Model model) {
-    List<Comic> topComics = comicMapper.findTopComics();
-    model.addAttribute("topComics", topComics);
+    List<Comic> popularComics = comicMapper.findTopComics();
+    List<Comic> recentComics = comicMapper.findRecentComics();
+    model.addAttribute("popularComics", popularComics);
+    model.addAttribute("recentComics", recentComics);
     return "home/home";
 }
 }
